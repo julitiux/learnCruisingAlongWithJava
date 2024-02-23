e9a9311591211bc38d2a67e4bdb77c2b93ed133b
Julio Ramirez <rrodriguez.julio@gmail.com>
2024-02-22 23:59:06 -0600

    we had to specify type information redundandlt on both sides when instantiating an object

--- app/src/main/java/learncruisingalongwithjava/typeinference/vsca/Diamond.java
+++ app/src/main/java/learncruisingalongwithjava/typeinference/vsca/Diamond.java
@@ -1,5 +1,11 @@
 package learncruisingalongwithjava.typeinference.vsca;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Diamond {
+
+  Map<String, List<Integer>> scores = new HashMap<String, List<Integer>>();
 
 }
