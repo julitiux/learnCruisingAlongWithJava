733c1c0d74b1a8e3d104616ec37428e21447c330
Julio Ramirez <rrodriguez.julio@gmail.com>
2024-03-05 00:35:16 -0600

    add diamond operator without casting

--- app/src/main/java/learncruisingalongwithjava/typeinference/vsca/Diamond.java
+++ app/src/main/java/learncruisingalongwithjava/typeinference/vsca/Diamond.java
@@ -12,4 +12,10 @@ public class Diamond {
    */
   Map<String, List<Integer>> scores = new HashMap<String, List<Integer>>();
 
+  /**
+   * imagine having to say darn thing over and over - that was the hard life before Java 7.
+   * Not enymore, thanks to the diamond operator.
+   */
+  Map<String, List<Integer>> scorez = new HashMap<>();
+
 }
