3e4a2aad344eb1944be9e6b40ef335840d5c7eb0
Julio Ramirez <rrodriguez.julio@gmail.com>
2024-03-05 23:32:07 -0600

    in teh lambda expression, we specified the type of the parameter as Integer

--- app/src/main/java/learncruisingalongwithjava/typeinference/vsca/Lambda.java
+++ app/src/main/java/learncruisingalongwithjava/typeinference/vsca/Lambda.java
@@ -4,6 +4,22 @@ import java.util.List;
 
 public class Lambda {
 
-  List<Integer> numbers = List.of(1,2,3,4,5,6,7,8,9,19);
+  final List<Integer> numbers = List.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 19);
+
+  public void lambda() {
+
+    /**
+     * in the lambda expression, we specified the type of the parameter as Integer,
+     * but in your wildest imagination.
+     */
+    numbers.forEach((Integer number) -> System.out.println(number * 2));
+
+    /**
+     * the compiler clearly knows the type in this context so do we. We can
+     * drop the type information from in front of the parameter name like so:
+     */
+    numbers.forEach((number) -> System.out.println(number * 2));
+
+  }
 
 }
