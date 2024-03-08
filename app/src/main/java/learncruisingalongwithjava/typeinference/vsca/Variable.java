4d598fee42abeb2a202b31d3e88326ba275a6dad
Julio Ramirez <rrodriguez.julio@gmail.com>
2024-03-07 20:50:41 -0600

    the compiler figures out that the variable is of type String based on thats assigned to it and doesnt need us to specify the type explicity

--- app/src/main/java/learncruisingalongwithjava/typeinference/vsca/Variable.java
+++ app/src/main/java/learncruisingalongwithjava/typeinference/vsca/Variable.java
@@ -3,7 +3,7 @@ package learncruisingalongwithjava.typeinference.vsca;
 public class Variable {
 
   public static void greet() {
-    String message = "hello there";
+    var message = "hello there";
     System.out.println(message);
   }
 
