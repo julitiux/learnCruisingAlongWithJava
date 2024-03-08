a90c66ff592bae921cd4857ed4351e2e18b1ac2c
Julio Ramirez <rrodriguez.julio@gmail.com>
2024-03-07 20:45:51 -0600

    in the code that follows we are providing explicit type information for the message variable

---
+++ app/src/main/java/learncruisingalongwithjava/typeinference/vsca/Variable.java
@@ -0,0 +1,10 @@
+package learncruisingalongwithjava.typeinference.vsca;
+
+public class Variable {
+
+  public static void greet() {
+    String message = "hello there";
+    System.out.println(message);
+  }
+
+}
