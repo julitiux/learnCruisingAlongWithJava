277996f151f00088103f6ada4c302f32f33888c7
Julio Ramirez <rrodriguez.julio@gmail.com>
2024-03-07 20:52:44 -0600

    consider the following

--- app/src/main/java/learncruisingalongwithjava/typeinference/vsca/Variable.java
+++ app/src/main/java/learncruisingalongwithjava/typeinference/vsca/Variable.java
@@ -1,10 +1,16 @@
 package learncruisingalongwithjava.typeinference.vsca;
 
+import java.util.HashMap;
+import java.util.List;
+
 public class Variable {
 
   public static void greet() {
     var message = "hello there";
     System.out.println(message);
+
+    // consider the following
+    HashMap<String, List<Integer>> scores = new HashMap<String, List<Integer>>();
   }
 
 }
