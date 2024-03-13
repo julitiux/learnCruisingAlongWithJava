79d18937e14a0e993e5aa63b1aa0e5e3ad1495d0
Julio Ramirez <rrodriguez.julio@gmail.com>
2024-03-12 22:41:16 -0600

    examine what type the compiler infers under the hood for a type inferred variable

---
+++ app/src/main/java/learncruisingalongwithjava/typeinference/vsca/TypeInferred.java
@@ -0,0 +1,11 @@
+package learncruisingalongwithjava.typeinference.vsca;
+
+public class TypeInferred {
+
+  public static void main(String[] args){
+    var message = "hello there";
+    var max = 1000;
+    var instance = new TypeInferred();
+  }
+
+}
